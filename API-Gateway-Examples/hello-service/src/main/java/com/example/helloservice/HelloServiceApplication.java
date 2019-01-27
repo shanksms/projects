3102823bb2db71a5bdc4d515652e68906c52d50b
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-01-27 14:59:11 +0530

    API Gateway example

---
+++ API-Gateway-Examples/hello-service/src/main/java/com/example/helloservice/HelloServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String goodBye() {
+		return "good Bye";
+	}
+
+}
+
